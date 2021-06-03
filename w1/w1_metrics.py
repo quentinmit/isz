a33508d0f5307cabd55fb0f6d1a710b00e8a4560
Quentin Smith <quentin@mit.edu>
2021-06-03 07:17:36 +0000

    Script for fetching 1-Wire temperatures

---
+++ w1/w1_metrics.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+from influxdb_client import Point
+import argparse
+import logging
+import sys
+import time
+from w1thermsensor import W1ThermSensor
+
+def main():
+    parser = argparse.ArgumentParser(description='Extract edgeos metrics.')
+    parser.add_argument('--verbose', action='store_true')
+
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.NOTSET if args.verbose else logging.INFO)
+
+    for line in sys.stdin:
+        for sensor in W1ThermSensor.get_available_sensors():
+            logging.debug("Found sensor %s", sensor)
+            print(Point("temp")
+                  .tag("chip", "w1")
+                  .tag("type_name", sensor.name)
+                  .tag("type_id", sensor.type.value)
+                  .tag("sensor", sensor.id)
+                  .field('temp', sensor.get_temperature())
+                  .time(time.time_ns())
+                  .to_line_protocol())
+
+
+if __name__ == "__main__":
+    main()
