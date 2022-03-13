1e9772679af86ff85c037b4beb668d6df5a7fcec
Quentin Smith <quentin@mit.edu>
2022-03-13 01:51:16 -0500

    Start to make graph.py reusable

--- workshop/dashboard/graph.py
+++ workshop/dashboard/graph.py
@@ -2,6 +2,7 @@
 
 from datetime import datetime, date, time, timedelta
 import io
+from itertools import chain
 import logging
 import os
 import sys
@@ -22,17 +23,21 @@ mpl.rc("axes", unicode_minus=False)
 
 TZ = ZoneInfo('America/New_York')
 
-client = InfluxDBClient(url="https://influx.isz.wtf", token=os.getenv("INFLUX_TOKEN"), org="icestationzebra")
+class Grapher:
+    def __init__(self):
+        self.client = InfluxDBClient(url="https://influx.isz.wtf", token=os.getenv("INFLUX_TOKEN"), org="icestationzebra")
 
-query_api = client.query_api()
+        self.query_api = self.client.query_api()
 
-p = {
-    "defaultBucket": "icestationzebra",
-    "windowPeriod": timedelta(minutes=10),
-    "timeRangeStart": timedelta(hours=-30),
-    "timeRangeStop": timedelta(days=3),
-}
-tables = query_api.query("""
+        self.p = {
+            "defaultBucket": "icestationzebra",
+            "windowPeriod": timedelta(minutes=10),
+            "timeRangeStart": timedelta(hours=-30),
+            "timeRangeStop": timedelta(days=3),
+        }
+
+    def fetch_weathergram(self):
+        tables = self.query_api.query("""
 import "experimental"
 import "strings"
 
@@ -65,62 +70,61 @@ from(bucket: defaultBucket)
   //|> group(columns: ["_start", "_stop", "_measurement", "_field"])
   //|> drop(columns: ["sensor", "name", "host"])
   |> yield(name: "local")
-""", params=p)
+""", params=self.p)
 
-gp = io.StringIO()
+        out = {}
 
-for table in tables:
-    result = table.records[0]['result']
-    if result == "local":
-        name = "localtemp"
-    elif result == "accuweather":
-        name = "aw"+table.records[0]['_field']
-    print(f"${name} << EOD", file=gp)
-    for record in table.records:
-        print(record["_time"].timestamp(), record["_value"], file=gp)
-    print("EOD", file=gp)
+        for table in tables:
+            result = table.records[0]['result']
+            if result == "local":
+                name = "localtemp"
+            elif result == "accuweather":
+                name = "aw"+table.records[0]['_field']
+            times = []
+            values = []
+            for record in table.records:
+                times.append(record["_time"])
+                values.append(record["_value"])
+            out[name] = (times, values)
+        return out
 
-#set format x2 "%A" timedate
+    def plot_weathergram(self):
+        tables = self.fetch_weathergram()
+        timestamps = list(chain.from_iterable(times for times,_ in tables.values()))
+        logging.debug("time = %s - %s", min(timestamps), max(timestamps))
 
+        xmin = datetime.now() - timedelta(hours=24)
 
-timestamps = [record["_time"] for table in tables for record in table]
-logging.debug("time = %s - %s", min(timestamps), max(timestamps))
+        fig = plt.figure(subplotpars=mpl.figure.SubplotParams(0,0,1,1))
+        ax = fig.add_axes((0,0,1,1), axes_class=axisartist.Axes)
+        ax.set_xlim(left=xmin, right=max(timestamps))
+        ax.xaxis.set_major_locator(mdates.DayLocator(tz=TZ))
+        ax.xaxis.set_minor_locator(mdates.HourLocator(interval=6, tz=TZ))
+        ax.xaxis.set_major_formatter(mdates.DateFormatter(' %A'))
+        ax.yaxis.set_major_formatter("{x:.0f}°C")
+        ax.axis[:].invert_ticklabel_direction()
+        ax.axis[:].major_ticks.set_tick_out(True)
+        ax.axis[:].minor_ticks.set_tick_out(True)
+        ax.axis[:].major_ticks.set_ticksize(8)
+        ax.axis[:].minor_ticks.set_ticksize(6)
+        ax.axis["top"].major_ticklabels.set_visible(True)
+        ax.axis["top"].major_ticklabels.set_ha("left")
+        ax.axis["top"].major_ticklabels.set_pad(0)
+        ax.axis["bottom"].major_ticklabels.set_visible(False)
+        ax.axis["top"].major_ticklabels.set_fontfamily("knxt")
+        ax.axis["left"].major_ticklabels.set_fontfamily("clean")
 
-xmin = datetime.now() - timedelta(hours=24)
+        for name, (times, values) in tables.items():
+            ax.plot(times, values, linewidth=1.5 if name == "localtemp" else 0.8)
+        ax.grid(axis='x', linestyle='dotted')
 
-fig = plt.figure(subplotpars=mpl.figure.SubplotParams(0,0,1,1))
-ax = fig.add_axes((0,0,1,1), axes_class=axisartist.Axes)
-ax.set_xlim(left=xmin, right=max(timestamps))
-ax.xaxis.set_major_locator(mdates.DayLocator(tz=TZ))
-ax.xaxis.set_minor_locator(mdates.HourLocator(interval=6, tz=TZ))
-ax.xaxis.set_major_formatter(mdates.DateFormatter(' %A'))
-ax.yaxis.set_major_formatter("{x:.0f}°C")
-ax.axis[:].invert_ticklabel_direction()
-ax.axis[:].major_ticks.set_tick_out(True)
-ax.axis[:].minor_ticks.set_tick_out(True)
-ax.axis[:].major_ticks.set_ticksize(8)
-ax.axis[:].minor_ticks.set_ticksize(6)
-ax.axis["top"].major_ticklabels.set_visible(True)
-ax.axis["top"].major_ticklabels.set_ha("left")
-ax.axis["top"].major_ticklabels.set_pad(0)
-ax.axis["bottom"].major_ticklabels.set_visible(False)
-ax.axis["top"].major_ticklabels.set_fontfamily("knxt")
-ax.axis["left"].major_ticklabels.set_fontfamily("clean")
-for table in tables:
-    result = table.records[0]['result']
-    if result == "local":
-        name = "localtemp"
-    elif result == "accuweather":
-        name = "aw"+table.records[0]['_field']
-    times = []
-    values = []
-    for record in table.records:
-        times.append(record["_time"])#.timestamp(),
-        values.append(record["_value"])
-    ax.plot(times, values, linewidth=1.5 if name == "localtemp" else 0.8)
-ax.grid(axis='x', linestyle='dotted')
+        return fig
 
-plt.savefig("out.pbm", format='pbm')
+if __name__ == '__main__':
+    g = Grapher()
+    fig = g.plot_weathergram()
+    fig.set_size_inches(1024/fig.dpi, 100/fig.dpi)
+    plt.savefig("out.png", format='png')
 #b = io.BytesIO()
 #plt.savefig(b, format='pbm')
 #print(b.getvalue())
