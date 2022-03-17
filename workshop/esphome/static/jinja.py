a24c130071857081f39d5eaced231737d947d68c
Quentin Smith <quentin@mit.edu>
2022-03-16 23:16:23 -0400

    Add select for switching TV inputs

--- workshop/esphome/static/jinja.py
+++ workshop/esphome/static/jinja.py
@@ -1,7 +1,12 @@
 import ionit_plugin
+import more_itertools
 
 @ionit_plugin.function
 def necformat(low, high=None):
     if high is None:
         high = ~low & 0xFF
     return (high << 8) + low
+
+@ionit_plugin.function
+def flatten(l):
+    return list(more_itertools.collapse(l))
