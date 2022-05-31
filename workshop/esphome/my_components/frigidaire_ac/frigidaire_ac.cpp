85d268bf86695ea719ac58828c152c54d69a24fe
Quentin Smith <quentin@mit.edu>
2022-05-31 00:58:44 -0400

    Add climate component for Frigidaire AC

---
+++ workshop/esphome/my_components/frigidaire_ac/frigidaire_ac.cpp
@@ -0,0 +1,144 @@
+#include "frigidaire_ac.h"
+
+namespace esphome {
+namespace frigidaire_ac {
+
+void FrigidaireACClimate::setup() {
+  if (this->temperature_sensor_) {
+    this->temperature_sensor_->add_on_state_callback([this](float state) {
+      this->current_temperature = state;
+      // current temperature changed, publish state
+      this->publish_state();
+    });
+    this->current_temperature = this->temperature_sensor_->state;
+  } else {
+    this->current_temperature = NAN;
+  }
+  this->mode = climate::CLIMATE_MODE_OFF;
+  this->preset = climate::CLIMATE_PRESET_NONE;
+  if (this->power_sensor_) {
+    this->power_sensor_->add_on_state_callback([this](float power) {
+      bool update_mode = millis() > (last_change_time_ + power_settling_time_);
+      if (power > 150) {
+        this->action = climate::CLIMATE_ACTION_COOLING;
+        if (update_mode) {
+          this->mode = climate::CLIMATE_MODE_COOL;
+        }
+      } else if (power > 50) {
+        this->action = climate::CLIMATE_ACTION_FAN;
+        if (update_mode && this->mode == climate::CLIMATE_MODE_OFF) {
+          this->mode = climate::CLIMATE_MODE_FAN_ONLY;
+        }
+      } else {
+        this->action = climate::CLIMATE_ACTION_IDLE;
+      }
+      this->publish_state();
+    });
+  }
+  auto restore = this->restore_state_();
+  if (restore.has_value()) {
+    restore->apply(this);
+  }
+  // This will be called by App.setup()
+}
+void FrigidaireACClimate::control(const climate::ClimateCall &call) {
+  bool changed = false;
+  climate::ClimateMode new_mode = this->mode;
+  climate::ClimatePreset new_preset = *this->preset;
+  if (call.get_mode().has_value()) {
+    new_mode = *call.get_mode();
+    new_preset = climate::CLIMATE_PRESET_NONE;
+  }
+  if (call.get_preset().has_value()) {
+    switch (*call.get_preset()) {
+    case climate::CLIMATE_PRESET_ECO:
+      new_mode = climate::CLIMATE_MODE_COOL;
+      new_preset = climate::CLIMATE_PRESET_ECO;
+      break;
+    case climate::CLIMATE_PRESET_NONE:
+      new_preset = climate::CLIMATE_PRESET_NONE;
+      break;
+    default:
+      ESP_LOGW(TAG, "Unsupported preset");
+      break;
+    }
+  }
+  if (new_mode != this->mode || new_preset != this->preset) {
+    // Send mode to hardware
+    if (this->mode == climate::CLIMATE_MODE_OFF) {
+      send_ir_(IR::POWER);
+    }
+    if (new_preset == climate::CLIMATE_PRESET_ECO) {
+      send_ir_(IR::MODE_ESAVE);
+    } else {
+      switch (new_mode) {
+      case climate::CLIMATE_MODE_COOL:
+        send_ir_(IR::MODE_COOL);
+        break;
+      case climate::CLIMATE_MODE_FAN_ONLY:
+        send_ir_(IR::MODE_FAN);
+        break;
+      case climate::CLIMATE_MODE_OFF:
+        send_ir_(IR::POWER);
+        break;
+      default:
+        ESP_LOGW(TAG, "Unsupported mode %d", new_mode);
+      }
+    }
+
+    // Publish updated state
+    this->mode = new_mode;
+    this->preset = new_preset;
+    changed = true;
+  }
+  if (call.get_target_temperature().has_value()) {
+    // User requested target temperature change
+    float temp = *call.get_target_temperature();
+    int8_t tempf = celsius_to_fahrenheit(temp);
+    // Send target temp to climate
+    if (0) {
+      // Send delta
+    } else {
+      send_ir_(IR::TEMP_DOWN, TEMPF_MAX-TEMPF_MIN, 50000);
+      uint8_t up_steps = tempf-TEMPF_MIN;
+      ESP_LOGD(TAG, "sending %d UP steps", up_steps);
+      send_ir_(IR::TEMP_UP, up_steps, 50000);
+    }
+    this->target_temperature = fahrenheit_to_celsius(tempf);
+    this->target_temperature_f = tempf;
+    changed = true;
+  }
+  if (call.get_fan_mode().has_value()) {
+    climate::ClimateFanMode fan_mode = *call.get_fan_mode();
+    ESP_LOGD(TAG, "fan_mode before: 0x%02X new: 0x%02X", *this->fan_mode, fan_mode);
+    switch (fan_mode) {
+    case climate::CLIMATE_FAN_AUTO:
+      send_ir_(IR::FAN_DOWN, 1);
+      send_ir_(IR::FAN_AUTO);
+      break;
+    case climate::CLIMATE_FAN_HIGH:
+      send_ir_(IR::FAN_UP, 3);
+      break;
+    case climate::CLIMATE_FAN_MEDIUM:
+      send_ir_(IR::FAN_DOWN, 3);
+      send_ir_(IR::FAN_UP);
+      break;
+    case climate::CLIMATE_FAN_LOW:
+      send_ir_(IR::FAN_DOWN, 3);
+      break;
+    default:
+      // Unsupported fan modes
+      break;
+    };
+    this->fan_mode = fan_mode;
+    changed = true;
+    ESP_LOGD(TAG, "fan_mode after: 0x%02X", *(this->fan_mode));
+  }
+  if (changed) {
+    this->last_change_time_ = millis();
+    this->publish_state();
+  }
+}
+
+};
+};
---
+++ workshop/esphome/my_components/frigidaire_ac/frigidaire_ac.h
@@ -0,0 +1,104 @@
+#pragma once
+
+#include "esphome/components/climate/climate.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/remote_base/remote_base.h"
+#include "esphome/components/remote_transmitter/remote_transmitter.h"
+#include "esphome/components/remote_base/nec_protocol.h"
+
+namespace esphome {
+namespace frigidaire_ac {
+
+static const char *const TAG = "climate.frigidaire_ac";
+
+const uint16_t IR_ADDRESS = 0xF508;
+const uint8_t TEMPF_MIN = 60;
+const uint8_t TEMPF_MAX = 90;
+
+class FrigidaireACClimate : public Component, public climate::Climate {
+ private:
+  enum IR : uint8_t {
+    FAN_DOWN = 4,
+      FAN_UP = 1,
+      FAN_AUTO = 15,
+
+      MODE_ESAVE = 2,
+      MODE_FAN = 7,
+      MODE_COOL = 9,
+
+      TEMP_DOWN = 13,
+      TEMP_UP = 14,
+
+      POWER = 17,
+      };
+ public:
+  void setup() override;
+  void control(const climate::ClimateCall &call) override;
+
+  climate::ClimateTraits traits() override {
+    // The capabilities of the climate device
+    auto traits = climate::ClimateTraits();
+    traits.set_visual_min_temperature(fahrenheit_to_celsius(TEMPF_MIN));
+    traits.set_visual_max_temperature(fahrenheit_to_celsius(TEMPF_MAX));
+    traits.set_visual_temperature_step(1);
+    if (this->temperature_sensor_) {
+      traits.set_supports_current_temperature(true);
+    }
+    if (this->power_sensor_) {
+      traits.set_supports_action(true);
+    }
+    traits.set_supported_modes({
+        climate::CLIMATE_MODE_OFF,
+        climate::CLIMATE_MODE_COOL,
+        climate::CLIMATE_MODE_FAN_ONLY,
+      });
+    traits.set_supported_presets({
+        climate::CLIMATE_PRESET_NONE,
+        climate::CLIMATE_PRESET_ECO,
+      });
+    traits.set_supported_fan_modes({
+        climate::CLIMATE_FAN_AUTO,
+        climate::CLIMATE_FAN_LOW,
+        climate::CLIMATE_FAN_MEDIUM,
+        climate::CLIMATE_FAN_HIGH,
+      });
+    return traits;
+  }
+
+  void set_temperature_sensor(sensor::Sensor *sensor) {
+    temperature_sensor_ = sensor;
+  }
+  void set_power_sensor(sensor::Sensor *sensor) {
+    power_sensor_ = sensor;
+  }
+  void set_transmitter(remote_transmitter::RemoteTransmitterComponent *transmitter) {
+    this->transmitter_ = transmitter;
+  }
+  void set_power_settling_time(uint32_t ms) {
+    this->power_settling_time_ = ms;
+  }
+ protected:
+  sensor::Sensor *temperature_sensor_ = NULL;
+  sensor::Sensor *power_sensor_ = NULL;
+  remote_transmitter::RemoteTransmitterComponent *transmitter_;
+  uint32_t last_change_time_ = 0;
+  uint32_t power_settling_time_ = 0;
+  uint32_t target_temperature_f = 0;
+
+  void send_ir_(IR cmd, uint8_t count = 1, uint32_t send_wait = 10000) {
+    if (this->transmitter_ == NULL) {
+      ESP_LOGW(TAG, "missing IR transmitter");
+      return;
+    }
+    auto call = this->transmitter_->transmit();
+    remote_base::NECData data{};
+    data.address = IR_ADDRESS;
+    data.command = (uint8_t)cmd | (~(uint8_t)cmd)<<8;
+    remote_base::NECProtocol().encode(call.get_data(), data);
+    call.set_send_times(count);
+    call.set_send_wait(send_wait); // ms
+    call.perform();
+    }
+};
+}  // namespace frigidaire_ac
+}  // namespace esphome
