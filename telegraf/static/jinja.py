88fa9c71f41759ec40f77d151daee2efd55be339
Quentin Smith <quentin@mit.edu>
2023-03-08 15:32:02 -0500

    Handle rapl rollover better

--- telegraf/static/jinja.py
+++ telegraf/static/jinja.py
@@ -1,5 +1,12 @@
 import ionit_plugin
+import jinja2
+from markupsafe import Markup
 
 @ionit_plugin.function
 def topython(i):
     return repr(i)
+
+@ionit_plugin.function
+@jinja2.pass_environment
+def include_raw(env: jinja2.Environment, name: str):
+    return Markup(env.loader.get_source(env, name)[0])
