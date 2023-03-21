f35184e19272010be9a751a4b7d41559c8fb9132
Quentin Smith <quentin@mit.edu>
2023-03-21 17:12:32 -0400

    Use sops-nix

--- flake.nix
+++ flake.nix
@@ -1,8 +1,14 @@
 {
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    sops-nix.url = "github:Mic92/sops-nix";
+  };
+  outputs = { self, nixpkgs, sops-nix }: {
     nixosConfigurations.workshop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./workshop/configuration.nix ];
+      modules = [
+        ./workshop/configuration.nix
+        sops-nix.nixosModules.sops
+      ];
     };
   };
 }
