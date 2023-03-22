e3b82492418c5d688359430156ada0a14caec6c1
Quentin Smith <quentin@mit.edu>
2023-03-22 16:41:13 -0400

    Support flake-compat

--- flake.nix
+++ flake.nix
@@ -1,17 +1,31 @@
 {
   inputs = {
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
     unstable.url = "nixpkgs/nixos-unstable";
     sops-nix.url = "github:Mic92/sops-nix";
-    nix-npm-buildpackage = "github:serokell/nix-npm-buildpackage";
+    nix-npm-buildpackage.url = "github:serokell/nix-npm-buildpackage";
   };
-  outputs = { self, nixpkgs, unstable, sops-nix }:
+  outputs = { self, nixpkgs, unstable, sops-nix, nix-npm-buildpackage, flake-compat }:
     let
       overlay = final: prev: {
         unstable = import unstable { inherit (prev) system; config.allowUnfree = true; };
       };
       # Overlays-module makes "pkgs.unstable" available in configuration.nix
-      overlayModule = ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay ]; });
+      overlayModule = ({ config, pkgs, ... }: {
+        nixpkgs.overlays = [
+          overlay
+          nix-npm-buildpackage.overlays.default
+        ];
+      });
     in {
+      pkgs = (import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [
+        (import ./nix/pkgs/all-packages.nix)
+      ];}).pkgs;
       nixosConfigurations.workshop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs.channels = { inherit nixpkgs unstable; };
