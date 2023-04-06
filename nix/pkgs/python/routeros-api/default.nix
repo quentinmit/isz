17e48189ab2ee966c93aa1e3f825ccab5d114ab2
Quentin Smith <quentin@mit.edu>
2023-04-06 16:18:39 -0400

    Fix packages

--- nix/pkgs/python/routeros-api/default.nix
+++ nix/pkgs/python/routeros-api/default.nix
@@ -1,17 +1,16 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, stdenv
 , pkgs
 , setuptools-scm
 , six
 }:
 
-buildPythonPackage {
+buildPythonPackage rec {
   pname = "RouterOS-api";
   version = "0.17.0";
 
-  src = python3.pkgs.fetchPypi {
+  src = fetchPypi {
     inherit pname version;
     hash = "sha256-G5iYRg7MRme1Tkd9SVt0wvJK4KrEyQ3Q5i8j7H6uglI=";
   };
--- nix/pkgs/python/w1thermsensor/default.nix
+++ nix/pkgs/python/w1thermsensor/default.nix
@@ -1,7 +1,6 @@
 { lib
 , buildPythonPackage
 , fetchPypi
-, stdenv
 , pkgs
 , setuptools-scm
 , aiofiles
