e9103c9e34dd69326cb0e7d8a3a08edc19023a88
Quentin Smith <quentin@mit.edu>
2023-04-28 01:43:44 -0400

    esphome: Use CONF_ID as object_id

--- nix/pkgs/unstable-overlays.nix
+++ nix/pkgs/unstable-overlays.nix
@@ -1,5 +1,8 @@
 final: prev: {
   esphome = prev.esphome.overrideAttrs (old: {
+    patches = (old.patches or []) ++ [
+      ./esphome/object-id-from-id.patch
+    ];
     meta.platforms = old.meta.platforms ++ final.lib.platforms.darwin;
   });
   esptool_3 = prev.esptool_3.overrideAttrs (old: {
