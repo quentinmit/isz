1123663c650148de90c5f6b30a29327e37d0fe35
Quentin Smith <quentin@mit.edu>
2023-04-29 00:03:11 -0400

    Install Alacritty

--- mac/configuration.nix
+++ mac/configuration.nix
@@ -466,6 +466,56 @@
       };
     };
 
+    programs.alacritty = {
+      enable = true;
+      package = pkgs.unstable.alacritty;
+      settings = {
+        font.normal.family = "Monaco";
+        font.size = 10;
+        font.offset = { x = 1; y = 1; };
+        draw_bold_text_with_bright_colors = true;
+        colors.primary.background = "0x000000";
+        colors.primary.foreground = "0xeaeaea";
+        colors.cursor.cursor = "0x4d4d4d";
+        colors.normal = {
+          black =   "0x000000";
+          red =     "0xd54e53";
+          green =   "0xb9ca4a";
+          yellow =  "0xe6c547";
+          blue =    "0x7aa6da";
+          magenta = "0xc397d8";
+          cyan =    "0x70c0ba";
+          white =   "0xeaeaea";
+        };
+        colors.bright = {
+          black =   "0x666666";
+          red =     "0xff3334";
+          green =   "0x9ec400";
+          yellow =  "0xe7c547";
+          blue =    "0x7aa6da";
+          magenta = "0xb77ee0";
+          cyan =    "0x54ced6";
+          white =   "0xffffff";
+        };
+        bell = {
+          duration = 100;
+          color = "0x888888";
+        };
+        window.padding = { x = 2; y = 2; };
+        window.opacity = 0.85;
+        window.decorations = "full";
+        window.option_as_alt = "Both";
+        selection.save_to_clipboard = false;
+        mouse_bindings = [
+          { mouse = "Middle"; action = "PasteSelection"; }
+        ];
+        key_bindings = [
+          { key = "N"; mods = "Command"; action = "SpawnNewInstance"; }
+          { key = "G"; mods = "Control"; mode = "Search"; action = "SearchCancel"; }
+        ];
+      };
+    };
+
     targets.darwin.defaults = {
       NSGlobalDefaults = {
         AppleInterfaceStyle = "Dark";
