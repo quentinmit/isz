e17377591959c23f0f5230fa8eaaa6016ad44942
Quentin Smith <quentin@mit.edu>
2023-05-31 16:13:09 -0400

    Initial test program

---
+++ nix/modules/telegraf/systemd-metrics/src/main.rs
@@ -0,0 +1,56 @@
+use async_std;
+use zbus::{dbus_proxy, Connection, Result, zvariant::{Type, OwnedObjectPath}};
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Type, Serialize, Deserialize)]
+pub struct UnitStatus {
+    /// The primary unit name as string
+    pub name: String,
+    /// The human readable description string
+    pub description: String,
+    /// The load state (i.e. whether the unit file has been loaded successfully)
+    pub load_state: String,
+    /// The active state (i.e. whether the unit is currently started or not)
+    pub active_state: String,
+    /// The sub state (a more fine-grained version of the active state that is specific to the unit type, which the active state is not)
+    pub sub_state: String,
+    /// A unit that is being followed in its state by this unit, if there is any, otherwise the empty string.
+    pub followed: String,
+    /// The unit object path
+    pub path: OwnedObjectPath,
+    /// If there is a job queued for the job unit the numeric job id, 0 otherwise
+    pub job_id: u32,
+    /// The job type as string
+    pub job_type: String,
+    /// The job object path
+    pub job_path: OwnedObjectPath,
+}
+
+#[dbus_proxy(
+    interface = "org.freedesktop.systemd1.Manager",
+    default_service = "org.freedesktop.systemd1",
+    default_path = "/org/freedesktop/systemd1"
+)]
+trait SystemdManager {
+    #[dbus_proxy(property)]
+    fn architecture(&self) -> Result<String>;
+    #[dbus_proxy(property)]
+    fn environment(&self) -> Result<Vec<String>>;
+
+    fn list_units(&self) -> Result<Vec<UnitStatus>>;
+}
+
+#[async_std::main]
+async fn main() -> Result<()> {
+    let connection = Connection::system().await?;
+
+    let proxy = SystemdManagerProxy::new(&connection).await?;
+    println!("Host architecture: {}", proxy.architecture().await?);
+    println!("Environment:");
+    for env in proxy.environment().await? {
+        println!("  {}", env);
+    }
+
+    Ok(())
+}
+
