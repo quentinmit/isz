a560c0a8d5b19a9d5818ea7db3617b0d02c1a354
Quentin Smith <quentin@mit.edu>
2023-06-01 14:51:16 -0400

    Connect to private socket when possible

--- nix/modules/telegraf/systemd-metrics/src/main.rs
+++ nix/modules/telegraf/systemd-metrics/src/main.rs
@@ -1,9 +1,9 @@
 use async_std;
-use zbus::{self, dbus_proxy, Connection, Result, zvariant::{Type, OwnedObjectPath}, fdo::PropertiesProxy};
+use zbus::{self, dbus_proxy, Connection, ConnectionBuilder, Result, zvariant::{Type, OwnedObjectPath}, fdo::PropertiesProxy};
 use zbus_names::InterfaceName;
 use serde::{Serialize, Deserialize};
 use std::time::SystemTime;
-use futures::future::FutureExt;
+use futures::future::{self, FutureExt, TryFutureExt};
 use futures::stream::{self, StreamExt, TryStreamExt, FuturesUnordered};
 
 #[derive(Debug, Type, Serialize, Deserialize)]
@@ -53,11 +53,21 @@ fn timeit<F: Fn() -> T, T>(f: F) -> T {
     result
 }
 
+async fn connect() -> Result<Connection> {
+    future::ready(ConnectionBuilder::address("unix:path=/run/systemd/private"))
+        .and_then(|b| b.p2p().build())
+        .or_else(|e| {
+            println!("private connection failed: {:?}", e);
+            Connection::system()
+        }).await
+}
+
 #[async_std::main]
 async fn main() -> Result<()> {
-    let connection = Connection::system().await?;
 
     let all_properties = false;
+
+    let connection = connect().await?;
 
     let proxy = SystemdManagerProxy::new(&connection).await?;
     println!("Host architecture: {}", proxy.architecture().await?);
@@ -68,30 +78,30 @@ async fn main() -> Result<()> {
     println!("Units:");
     timeit(|| async_std::task::block_on(async {
         let units = proxy.list_units().await?;
-	stream::iter(units).map(Ok).try_for_each_concurrent(10, |unit| {
-	let conn = &connection;
-	let (name, path) = (unit.name, unit.path);
-	async move {
-            println!(" {} - {}", name, path);
-            let properties_proxy = PropertiesProxy::builder(conn).destination("org.freedesktop.systemd1")?.path(path.clone())?.build().await?;
-            if all_properties {
-                let properties = properties_proxy.get_all(InterfaceName::from_static_str_unchecked("")).await?;
-                for (key, value) in &properties {
-                    println!("  {}={:?}", key, value);
+        stream::iter(units).map(Ok).try_for_each_concurrent(10, |unit| {
+            let conn = &connection;
+            let (name, path) = (unit.name, unit.path);
+            async move {
+                println!(" {} - {}", name, path);
+                let properties_proxy = PropertiesProxy::builder(conn).destination("org.freedesktop.systemd1")?.path(path.clone())?.build().await?;
+                if all_properties {
+                    let properties = properties_proxy.get_all(InterfaceName::from_static_str_unchecked("")).await?;
+                    for (key, value) in &properties {
+                        println!("  {}={:?}", key, value);
+                    }
                 }
-            }
-            let unit_type = name.rsplit_once(".").map(|(_, v)| v).unwrap_or(&name);
-            let interface_name = InterfaceName::try_from(
-                format!("org.freedesktop.systemd1.{}", unit_type.chars().next().map(|c| c.to_uppercase().chain(unit_type.chars().skip(1)).collect::<String>()).unwrap())
-            )?;
-            for key in ["ControlGroup", "IPIngressBytes", "IPIngressPackets", "IPEgressBytes", "IPEgressPackets"] {
-                properties_proxy.get(interface_name.clone(), key).await.map_or_else(
-                    |e| println!("  {} error {:?}", key, e),
-                    |value| println!("  {}={:?}", key, value)
-                );
-            }
-	    Ok::<(), zbus::Error>(())
-        }}).await
+                let unit_type = name.rsplit_once(".").map(|(_, v)| v).unwrap_or(&name);
+                let interface_name = InterfaceName::try_from(
+                    format!("org.freedesktop.systemd1.{}", unit_type.chars().next().map(|c| c.to_uppercase().chain(unit_type.chars().skip(1)).collect::<String>()).unwrap())
+                )?;
+                for key in ["ControlGroup", "IPIngressBytes", "IPIngressPackets", "IPEgressBytes", "IPEgressPackets"] {
+                    properties_proxy.get(interface_name.clone(), key).await.map_or_else(
+                        |e| println!("  {} error {:?}", key, e),
+                        |value| println!("  {}={:?}", key, value)
+                    );
+                }
+                Ok::<(), zbus::Error>(())
+            }}).await
     }))?;
 
     Ok(())
