ad2a348aaa5aa4adb7e2eb10a7c8959962486b19
Quentin Smith <quentin@mit.edu>
2023-06-10 18:06:55 -0400

    Configure gtk and git

--- steamdeck/deck.nix
+++ steamdeck/deck.nix
@@ -36,4 +36,37 @@
       if [ -d "/home/deck/stl/prefix" ]; then export PATH="$PATH:/home/deck/stl/prefix"; fi
     '';
   };
+  gtk = {
+    enable = true;
+    theme.name = "Breeze";
+    cursorTheme.name = "breeze_cursors";
+    cursorTheme.size = 24;
+    iconTheme.name = "breeze-dark";
+    font.name = "Noto Sans";
+    font.size = 11;
+    gtk2.extraConfig = ''
+      gtk-enable-animations=1
+      gtk-primary-button-warps-slider=0
+      gtk-toolbar-style=3
+      gtk-menu-images=1
+      gtk-button-images=1
+    '';
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+      gtk-button-images = true;
+      gtk-decoration-layout = "icon:minimize,maximize,close";
+      gtk-enable-animations = true;
+      gtk-menu-images = true;
+      gtk-modules = "colorreload-gtk-module:window-decorations-gtk-module";
+      gtk-primary-button-warps-slider = false;
+      gtk-toolbar-style = 3;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+      gtk-decoration-layout = "icon:minimize,maximize,close";
+      gtk-enable-animations = true;
+      gtk-primary-button-warps-slider = false;
+    };
+  };
+  programs.git.lfs.enable = true;
 }
