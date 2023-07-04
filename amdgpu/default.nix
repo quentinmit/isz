2592e1ae3f38a5ad1bf9e6e5f17c249ff316d669
Quentin Smith <quentin@mit.edu>
2023-07-04 02:44:36 -0400

    Update cargo hash

--- amdgpu/default.nix
+++ amdgpu/default.nix
@@ -24,7 +24,7 @@ rustPlatform.buildRustPackage rec {
   src = ./.;
 
   #cargoSha256 = lib.fakeSha256;
-  cargoSha256 = "";
+  cargoSha256 = "SP//qLiA4ZQ0wqKqupD5ppFu4PG3z304BoUwDGOy4OM=";
 
   LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
   doCheck = false;
