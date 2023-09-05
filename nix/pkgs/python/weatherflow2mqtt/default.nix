d997b43f6f8cfb0c059ad41388538015430b1ad5
Quentin Smith <quentin@mit.edu>
2023-09-05 01:03:56 -0400

    Update to weatherflow2mqtt 3.2.1 from upstream

--- nix/pkgs/python/weatherflow2mqtt/default.nix
+++ nix/pkgs/python/weatherflow2mqtt/default.nix
@@ -5,13 +5,13 @@
 with python3Packages;
 buildPythonApplication rec {
   pname = "weatherflow2mqtt";
-  version = "3.1.7";
+  version = "3.2.1";
 
   src = fetchFromGitHub {
-    owner = "quentinmit";
+    owner = "briis";
     repo = "hass-weatherflow2mqtt";
-    rev = "main";
-    sha256 = "sha256-WzY9w6NG4FF7p0NylnskANWfQdn0yvzD3FtppFEBGSg=";
+    rev = "4004687fbf257044e8922ecc3b20408bcc1d7d3f";
+    sha256 = "sha256-7lnsy8vlLWK11vwVTxIkc/lg0WBIanrLi6mfiKDX3wI=";
   };
 
   prePatch = ''
