a6aa0784a67d4f25ce6b5e850353cae5a14f9b35
Quentin Smith <quentin@golang.org>
2023-11-30 23:40:34 -0500

    equivs package works

--- nix/pkgs/equivs/default.nix
+++ nix/pkgs/equivs/default.nix
@@ -14,20 +14,25 @@ stdenv.mkDerivation rec {
 
   postPatch = ''
     substituteInPlace Makefile --replace pod2man ${perlPackages.perl}/bin/pod2man
-    substituteInPlace usr/bin/equivs-build usr/bin/equivs-control --replace /usr/share $out/share --replace dpkg-buildpackage ${dpkg}/bin/dpkg-buildpackage --replace "cp -R" "${rsync}/bin/rsync -rltE --chmod=u+w" --replace "cp " "cp --no-preserve=mode "
+    substituteInPlace usr/bin/equivs-build usr/bin/equivs-control --replace /usr/share $out/share --replace dpkg-buildpackage "${dpkg}/bin/dpkg-buildpackage --admindir $out/share/equivs/admindir" --replace "cp -R" "${rsync}/bin/rsync -rltE --chmod=u+w" --replace "cp " "cp --no-preserve=mode "
+    substituteInPlace usr/share/equivs/template/debian/rules --replace "dh \$@" "dh \$@ --without autoreconf"
+    cat >>usr/share/equivs/template/debian/rules <<EOF
+    override_dh_strip_nondeterminism:
+    EOF
     patchShebangs usr/bin
   '';
 
   # debhelper
   nativeBuildInputs = [ makeWrapper ];
-  propagatedBuildInputs = [ dpkg debhelper ];
-  buildInputs =
-    (with perlPackages; [ perl ]);
+  propagatedBuildInputs = [ dpkg debhelper perlPackages.perl ];
 
   installPhase = ''
     cp -R --preserve=mode usr $out
     mkdir -p $out/share/man/man1
     cp -R *.1 $out/share/man/man1
+    mkdir -p $out/share/equivs/admindir/info
+    mkdir -p $out/share/equivs/admindir/updates
+    touch $out/share/equivs/admindir/status
   '';
 
   meta = with lib; {
