11ad7c2a65d3495a3adf5eacfa9752c3f927646a
Quentin Smith <quentin@mit.edu>
2024-02-20 23:25:58 -0500

    Update BlueChips, fix infinite login loop

--- workshop/authentik/bluechips.nix
+++ workshop/authentik/bluechips.nix
@@ -45,9 +45,9 @@
     locations."@goauthentik_proxy_signin".extraConfig = ''
       internal;
       add_header Set-Cookie $auth_cookie;
-      #return 302 /outpost.goauthentik.io/start?rd=$request_uri;
+      return 302 /outpost.goauthentik.io/start?rd=$request_uri;
       # For domain level, use the below error_page to redirect to your authentik server with the full redirect path
-      return 302 https://auth.isz.wtf/outpost.goauthentik.io/start?rd=$scheme://$http_host$request_uri;
+      #return 302 https://auth.isz.wtf/outpost.goauthentik.io/start?rd=$scheme://$http_host$request_uri;
     '';
   };
 }
