40ef6099ac369475d0468de69f16692b33dcf087
Quentin Smith <quentin@mit.edu>
2024-02-23 19:50:50 -0500

    Store Paperless metadata in Postgres

--- workshop/paperless.nix
+++ workshop/paperless.nix
@@ -7,7 +7,16 @@
         GUNICORN_CMD_ARGS = "--bind=unix:/run/paperless/paperless.sock";
         PAPERLESS_URL = "https://paperless.isz.wtf";
         PAPERLESS_USE_X_FORWARD_HOST = true;
+        PAPERLESS_DBENGINE = "postgresql";
+        PAPERLESS_DBHOST = "/run/postgresql/";
+        PAPERLESS_DBNAME = config.services.paperless.user;
       };
+    };
+    services.postgresql = {
+      ensureDatabases = [ config.services.paperless.user ];
+      ensureUsers = [
+        { name = config.services.paperless.user; ensureDBOwnership = true; }
+      ];
     };
     systemd.services.paperless-web.serviceConfig = {
       RuntimeDirectory = "paperless";
