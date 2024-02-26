22389cf1368f643a693da86f19c2727fbb9c553b
Quentin Smith <quentin@mit.edu>
2024-02-25 22:49:24 -0500

    Switch to buildRustPackage to build systemd-metrics more quickly

--- nix/modules/telegraf/systemd-metrics/default.nix
+++ nix/modules/telegraf/systemd-metrics/default.nix
@@ -1,8 +1,25 @@
-{ pkgs, ... }:
+{ lib
+, rustPlatform
+, openssl
+, pkg-config
+}:
+
 let
-  rustPkgs = pkgs.rustBuilder.makePackageSet {
-    rustVersion = "1.70.0";
-    packageFun = import ./Cargo.nix;
-    # packageOverrides = pkgs: pkgs.rustBuilder.overrides.all; # Implied, if not specified
-  };
-in (rustPkgs.workspace.systemd-metrics {}).bin
+    cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+in
+rustPlatform.buildRustPackage rec {
+  pname = cargoToml.package.name;
+  inherit (cargoToml.package) version;
+
+  src = ./.;
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    openssl
+  ];
+
+  cargoHash = "sha256-Orsl9SCfR9bDG53BoFeEgU608o9j4lBhHmvQW4piP/U=";
+}
