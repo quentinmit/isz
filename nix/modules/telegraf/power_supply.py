ed28120f8b2845ff968f1e6f7fe53cf70eaae9e3
Quentin Smith <quentin@mit.edu>
2024-03-25 11:25:26 -0400

    Treat more fields as tags

--- nix/modules/telegraf/power_supply.py
+++ nix/modules/telegraf/power_supply.py
@@ -3,13 +3,22 @@ import os.path
 import sys
 import time
 ROOT = "/sys/class/power_supply"
+TAGS = {
+    "name",
+    "type",
+    "technology",
+    "scope",
+    "model_name",
+    "manufacturer",
+    "serial_number",
+}
 for line in sys.stdin:
     for device in os.listdir(ROOT):
         result = {}
         for line in open(os.path.join(ROOT, device, "uevent")):
             name, value = line.strip().split("=", 1)
             name = name.split("_", 2)[2].lower()
-            if name in {"name", "type", "technology"}:
+            if name in TAGS:
                 device += ",%s=%s" % (name, value)
                 continue
             try:
