70e498e7da6f7039280df5e82afdaf3640e4ba94
Quentin Smith <quentin@mit.edu>
2024-08-29 02:21:06 -0400

    Fix power supply metrics

--- nix/modules/telegraf/power_supply/telegraf.nix
+++ nix/modules/telegraf/power_supply/telegraf.nix
@@ -4,12 +4,12 @@
     isz.telegraf.powerSupply = mkEnableOption "power_supply";
   };
   config = {
-    services.telegraf.extraConfig = lib.mkIf config.isz.telegraf.w1 {
+    services.telegraf.extraConfig = lib.mkIf config.isz.telegraf.powerSupply {
       inputs.execd = [{
         alias = "power_supply";
         restart_delay = "10s";
         data_format = "influx";
-        command = [pkgs.iszTelegraf.powerSupply];
+        command = [pkgs.iszTelegraf.power_supply];
         signal = "STDIN";
       }];
     };
