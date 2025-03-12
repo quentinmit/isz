0d6a75bb92dcd34b728809c6fd70bc61fa3d0007
Quentin Smith <quentin@mit.edu>
2025-03-12 04:31:11 -0400

    Update wd-fw-update

--- nix/pkgs/python/wd-fw-update/default.nix
+++ nix/pkgs/python/wd-fw-update/default.nix
@@ -7,19 +7,12 @@
 with python3Packages;
 buildPythonApplication rec {
   pname = "wd_fw_update";
-  version = "1.2.1";
+  version = "2.1.0";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-iZdHQ9qOVyUY2/JPqO0TPvL/wQc2b/4tIUmEuJZhIq4=";
+    hash = "sha256-tWtO1uJGLjVqBnPLnKzFCMPBO92Oqqg5VMvjXpHZxNk=";
   };
-
-  patches = [
-    (fetchpatch2 {
-      url = "https://github.com/not-a-feature/wd_fw_update/pull/2.patch";
-      hash = "sha256-3RkhGMmcKuhiMpWgy/gfDD6NKZhWrupd7VZpmznNsn4=";
-    })
-  ];
 
   postPatch = ''
     substituteInPlace src/wd_fw_update/main.py --replace '"nvme"' '"${nvme-cli}/bin/nvme"'
