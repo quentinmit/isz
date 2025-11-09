8eddfbd952e54002d0c3a9765122a51e09970d28
Quentin Smith <quentin@mit.edu>
2025-11-09 00:26:43 -0500

    nix flake update

--- workshop/authentik/default.nix
+++ workshop/authentik/default.nix
@@ -13,11 +13,6 @@
         "authentik.service"
       ];
     };
-    systemd.services = lib.genAttrs [ "authentik-migrate" "authentik-worker" "authentik" ] (name: {
-      serviceConfig.SupplementaryGroups = [
-        config.services.redis.servers.authentik.user
-      ];
-    });
     services.authentik = {
       enable = true;
       environmentFile = config.sops.secrets."authentik/environment".path;
@@ -26,21 +21,11 @@
         enableACME = true;
         host = "auth.isz.wtf";
       };
-      settings = let
-        redisUrl = "unix://${config.services.redis.servers.authentik.unixSocket}?db=0";
-        redisSocketUrl = "redis+socket://${config.services.redis.servers.authentik.unixSocket}?db=0";
-      in {
+      settings = {
         error_reporting.enabled = false;
         disable_update_check = true;
         disable_startup_analytics = true;
         avatars = "gravatar,initials";
-        # https://github.com/jazzband/django-redis#configure-as-cache-backend
-        cache.url = redisUrl;
-        # https://github.com/django/channels_redis#hosts
-        channel.url = redisUrl;
-        # https://docs.celeryq.dev/en/stable/userguide/configuration.html#redis-backend-settings
-        broker.url = redisSocketUrl;
-        result_backend.url = redisSocketUrl;
         # Disable outpost discovery since there's no Kubernetes or Docker.
         outposts.discover = false;
       };
