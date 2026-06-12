348de5823872cc34a6c53e1f1e05124d00e48a3d
Quentin Smith <quentin@mit.edu>
2026-06-12 12:34:45 -0400

    Automatically add polkit rule for sensor access

--- nix/modules/telegraf/iio.nix
+++ nix/modules/telegraf/iio.nix
@@ -1,4 +1,4 @@
-{ lib, config, pkgs, ... }:
+{ lib, options, config, pkgs, ... }:
 {
   options = with lib; {
     isz.telegraf.iio.light = mkOption {
@@ -7,19 +7,33 @@
       description = "Whether to monitor an IIO light sensor";
     };
   };
-  config = {
-    services.telegraf.extraConfig = lib.mkIf config.isz.telegraf.iio.light {
-      inputs.execd = [{
-        alias = "iio";
-        name_override = "iio";
-        command = [(lib.getExe' pkgs.iio-sensor-proxy "monitor-sensor") "--light"];
-        restart_delay = "10s";
-        data_format = "grok";
-        grok_patterns = [
-          ''ambient light sensor \(value: %{NUMBER:light:float}, unit: %{WORD:unit:tag}\)'' # First line
-          ''    Light changed: %{NUMBER:light:float} \(%{WORD:unit:tag}\)'' # Subsequent lines
-        ];
-      }];
-    };
-  };
+  config = lib.mkMerge [
+    {
+      services.telegraf.extraConfig = lib.mkIf config.isz.telegraf.iio.light {
+        inputs.execd = [{
+          alias = "iio";
+          name_override = "iio";
+          command = [(lib.getExe' pkgs.iio-sensor-proxy "monitor-sensor") "--light"];
+          restart_delay = "10s";
+          data_format = "grok";
+          grok_patterns = [
+            ''ambient light sensor \(value: %{NUMBER:light:float}, unit: %{WORD:unit:tag}\)'' # First line
+            ''    Light changed: %{NUMBER:light:float} \(%{WORD:unit:tag}\)'' # Subsequent lines
+          ];
+        }];
+      };
+    }
+    (lib.optionalAttrs (options ? security.polkit) (lib.mkIf config.isz.telegraf.iio.light {
+      security.polkit.extraConfig = ''
+        polkit.addRule(function(action, subject) {
+          if (
+            action.id === "net.hadess.SensorProxy.claim-sensor"
+            && subject.user === "telegraf"
+          ) {
+            return polkit.Result.YES;
+          }
+        });
+      '';
+    }))
+  ];
 }
