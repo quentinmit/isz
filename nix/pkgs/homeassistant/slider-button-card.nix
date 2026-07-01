d3126c119790ab77f3870094b6634193b5387274
Quentin Smith <quentin@mit.edu>
2026-07-01 00:53:47 -0400

    slider-button-card: 1.11.0-beta -> 1.13.0

--- nix/pkgs/homeassistant/slider-button-card.nix
+++ nix/pkgs/homeassistant/slider-button-card.nix
@@ -5,14 +5,14 @@
 
 let
   pname = "slider-button-card";
-  version = "1.11.0-beta";
+  version = "1.13.0";
 in
 stdenv.mkDerivation {
   inherit pname version;
 
   src = fetchurl {
-    url = "https://github.com/mattieha/slider-button-card/releases/download/v${version}/slider-button-card.js";
-    hash = "sha256-2Q+DLZbqgz7il00E3im4uOt0W4FYcQ1TMCfxyQgL7o4=";
+    url = "https://github.com/custom-cards/slider-button-card/releases/download/v${version}/slider-button-card.js";
+    hash = "sha256-WF4gg4Jjc78cfNkhUC21+yYYuloTJ/8qadXWI5LZngQ=";
   };
 
   dontUnpack = true;
@@ -25,8 +25,8 @@ stdenv.mkDerivation {
 
   meta = with lib; {
     description = "Lovelace Slider button card";
-    homepage = "https://github.com/mattieha/slider-button-card";
-    maintainers = with maintainers; [ hexa ];
+    homepage = "https://github.com/custom-cards/slider-button-card";
+    maintainers = with maintainers; [ quentin ];
     license = licenses.mit;
   };
 }
