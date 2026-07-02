8af48fc8f0918bec0da6ad71b11f2d7d714de830
Quentin Smith <quentin@mit.edu>
2026-07-02 16:55:10 -0400

    Expose greptimedb

--- workshop/greptimedb.nix
+++ workshop/greptimedb.nix
@@ -3,4 +3,21 @@
   services.greptimedb = {
     enable = true;
   };
+  services.nginx = {
+    upstreams.greptimedb.servers."localhost:4000" = {};
+    virtualHosts."greptimedb.isz.wtf" = {
+      forceSSL = true;
+      enableACME = true;
+      locations."/" = {
+        proxyPass = "http://greptimedb";
+        proxyWebsockets = true;
+        # TODO: Configure authentication and allow external access
+        extraConfig = ''
+          proxy_buffering off;
+          allow 172.30.96.0/23;
+          deny all;
+        '';
+      };
+    };
+  };
 }
